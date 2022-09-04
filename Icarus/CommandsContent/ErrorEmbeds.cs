ccb034f542de026afd93771f9432ac6aa586b641
Arxaid <VallravenWasTaken@gmail.com>
2022-09-04 20:27:55 +0300

    Added error embeds for in-discord debug purposes

---
+++ Icarus/CommandsContent/ErrorEmbeds.cs
@@ -0,0 +1,32 @@
+﻿// This file is part of the Icarus project.
+//
+// Copyright (c) 2022 Vladislav Sosedov.
+
+using DSharpPlus.Entities;
+
+namespace Icarus.Commands
+{
+    internal static class ErrorEmbeds
+    {
+        public static DiscordEmbedBuilder databaseError_FireteamCreation = new DiscordEmbedBuilder
+        {
+            Title = "Database error.",
+            Description = "Fireteam creation.",
+            Color = DiscordColor.Red
+        };
+
+        public static DiscordEmbedBuilder databaseError_FireteamAddMember = new DiscordEmbedBuilder
+        {
+            Title = "Database error.",
+            Description = "Adding a member to existing fireteam.",
+            Color = DiscordColor.Red
+        };
+
+        public static DiscordEmbedBuilder databaseError_FireteamDeleteMember = new DiscordEmbedBuilder
+        {
+            Title = "Database error.",
+            Description = "Removal a member from existing fireteam.",
+            Color = DiscordColor.Red
+        };
+    }
+}
